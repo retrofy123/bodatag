c7ac926718b3925661269e61dfe66630303a64f5
retrofy123 <retrofy7@gmail.com>
2025-04-11 22:59:59 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -120,4 +120,4 @@ def get_title_data():
     return jsonify({"Error": "Failed to fetch title data"}), response.status_code
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=63717)
+    app.run(host="0.0.0.0", port=63718)
